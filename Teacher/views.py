dc9e921ec87cb4ef77765fbcfad3af45b10831be
febin <febinbabu2023a@mca.ajce.in>
2023-05-16 15:47:18 +0530

    sklearn package deleted

--- Teacher/views.py
+++ Teacher/views.py
@@ -9,8 +9,7 @@ from Schoolapp.views import Home
 import Teacher
 from django.contrib import messages
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+
 
 
 
